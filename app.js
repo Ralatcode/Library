40136f21bd4b7d2415d2b2eb1b7e3eae2faf7c8b
RalatPC <ralatrado@gmail.com>
2023-06-16 20:25:39 +0100

    looped sample array to display the book obj properites in the DOM

--- app.js
+++ app.js
@@ -2,7 +2,27 @@
 let obj1 = new Book('RDPD', 'Robert Kiyosaki and Sharon Lechter', 765, 'not read yet');
 let obj2 = new Book ('LDFH', 'A.H. Mohammed', 126, 'read');
 let obj3 = new Book ('Wired', 'Buchanan-Renard', 329, 'not read yet');
+
+const container = document.querySelector('.container');
+
 let myLibrary = [obj1, obj2, obj3];
+
+myLibrary.forEach(book =>{
+    // create all necesaary html tags for book details
+    const bookDiv = document.createElement('div');
+    const bookTitle = document.createElement('h2');
+    const bookAuthor = document.createElement('h3');
+    const bookPages = document.createElement('h4');
+    const bookStatus = document.createElement('h5');
+    // update the text of html tags to object properties
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = book.author;
+    bookPages.textContent = book.pages;
+    bookStatus.textContent = book.status;
+    // append tags to bookDiv
+    bookDiv.append(bookTitle, bookAuthor, bookPages, bookStatus);
+    container.appendChild(bookDiv);
+}); 
 
 
 
@@ -13,6 +33,7 @@ function Book(title, author, pages, status) {
     this.pages = pages
     this.status = status
 }
+
 
 // function createPrototype() {
 //     let newBook = new Book('RDPD', 'Robert Kiyosaki and Sharon Lechter', '765', 'not read yet');
